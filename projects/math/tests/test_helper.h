ffe37dbf32bdad272615e22d0fbb8f6de1190ac3
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-03-02 15:36:17 -0500

    doctest math rendering

--- projects/math/tests/test_helper.h
+++ projects/math/tests/test_helper.h
@@ -1,12 +1,24 @@
 #ifndef TOV_MATH_TESTS_TEST_HELPER_H
 #define TOV_MATH_TESTS_TEST_HELPER_H
 
-#include <catch2/catch_test_macros.hpp>
+#include <doctest.h>
+#include <cmath>
+
+#define SECTION(name) DOCTEST_SUBCASE(name)
+
+#undef TEST_CASE
+#define TEST_CASE(name, tags) DOCTEST_TEST_CASE(tags " " name)
+
+// catch exposes this by default outside of its namespace
+using doctest::Approx;
+
 
 inline bool cmpf(float a, float b, float epsilon = 1e-7)
 {
     if (std::abs(a - b) < epsilon) return true;
     return false;
 }
+
+#include <cstring>
 
 #endif
--- projects/rendering/tests/test_helper.h
+++ projects/rendering/tests/test_helper.h
@@ -1,9 +1,14 @@
 #ifndef TOV_RENDERING_TESTS_TEST_HELPER_H
 #define TOV_RENDERING_TESTS_TEST_HELPER_H
 
-#include <catch2/catch_test_macros.hpp>
-
+#include <doctest.h>
+#include <cstring>
 #include <iostream>
+
+#define SECTION(name) DOCTEST_SUBCASE(name)
+
+#undef TEST_CASE
+#define TEST_CASE(name, tags) DOCTEST_TEST_CASE(tags " " name)
 
 inline bool cmpf(float a, float b, float epsilon = 9e-7)
 {
