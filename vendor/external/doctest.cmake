5540ad169ad1309e1e78741212a54b3f8d7d8270
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-03-02 15:09:00 -0500

    rename catch with doctest

--- vendor/external/doctest.cmake
+++ vendor/external/doctest.cmake
@@ -1,13 +1,13 @@
 cmake_minimum_required (VERSION 3.8)
 
-project(catch NONE)
+project(doctest NONE)
 
 include(ExternalProject)
-ExternalProject_Add(catch
+ExternalProject_Add(doctest
   GIT_REPOSITORY    https://github.com/onqtam/doctest.git
   GIT_TAG           2.4.5
-  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/catch/src"
-  BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/catch/build"
+  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/doctest/src"
+  BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/doctest/build"
   CONFIGURE_COMMAND ""
   BUILD_COMMAND     ""
   INSTALL_COMMAND   ""
