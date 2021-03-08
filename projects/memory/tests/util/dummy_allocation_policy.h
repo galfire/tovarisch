f13bd73f3984a2038325f8666bf1bb685c2eddca
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-03-08 03:03:52 -0500

    Update dummy_allocation_policy.h

--- projects/memory/tests/util/dummy_allocation_policy.h
+++ projects/memory/tests/util/dummy_allocation_policy.h
@@ -12,23 +12,12 @@ namespace tov
             public:
                 void* allocate(size_t sz)
                 {
-                    void* ptr = operator new(sizeof(size_t) + sz);
-                    *static_cast<size_t*>(ptr) = sz;
-                    return static_cast<unsigned char*>(ptr) + sizeof(size_t);
+                    return operator new(sz);
                 }
 
                 void deallocate(void* ptr)
                 {
-                    void* originalPtr = static_cast<unsigned char*>(ptr) - sizeof(size_t);
-                    operator delete(originalPtr);
-                }
-
-                size_t getAllocationSize(void* ptr) const
-                {
-                    unsigned char* asByte = static_cast<unsigned char*>(ptr);
-                    asByte -= sizeof(size_t);
-                    size_t sz = *reinterpret_cast<size_t*>(asByte);
-                    return sz;
+                    operator delete(ptr);
                 }
 
                 void reset() {}
