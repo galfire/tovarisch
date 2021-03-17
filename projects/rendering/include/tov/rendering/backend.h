845c44eb9a3f2f616d11f84d0911361024c1e850
Brandon Vrooman <brandon.vrooman@gmail.com>
2021-03-17 03:50:52 -0400

    Update backend.h

--- projects/rendering/include/tov/rendering/backend.h
+++ projects/rendering/include/tov/rendering/backend.h
@@ -49,7 +49,7 @@ namespace tov
     ) -> RenderSystem*;
 
     auto createBufferManager() -> buffers::BufferManagerBase*;
-    auto createDrawDataContext() -> mesh::DrawDataContext*
+    auto createDrawDataContext() -> mesh::DrawDataContext*;
     auto createDrawDataContext(const mesh::VertexData& vertexData) -> mesh::DrawDataContext*;
     auto getDefaultFramebuffer() -> pipeline::Framebuffer*;
     auto createFramebuffer() -> pipeline::Framebuffer*;
